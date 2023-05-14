4a6d1df86134354ad97b020830cf4b994b3be611
Vishu26 <srikumarsastry1997@gmail.com>
2023-05-13 20:56:38 -0500

    updated scripts

--- CrossMAE.py
+++ CrossMAE.py
@@ -1,5 +1,5 @@
-from vit_pytorch import ViT
-import torch
+from .MAEPretrain_SceneClassification.models_mae_vitae import CrossViewMaskedAutoencoder
+import torch 
 import torch.nn as nn
 import pytorch_lightning as pl
 from pytorch_lightning import LightningModule
@@ -20,170 +20,61 @@ import pandas as pd
 from torchmetrics import Accuracy
 from datetime import datetime
 import copy
+import os
+from functools import partial
 
-class MAE(nn.Module):
-    def __init__(
-        self,
-        *,
-        encoder,
-        decoder_dim,
-        masking_ratio = 0.75,
-        decoder_depth = 1,
-        decoder_heads = 8,
-        decoder_dim_head = 64
-    ):
-        super().__init__()
-        assert masking_ratio > 0 and masking_ratio < 1, 'masking ratio must be kept between 0 and 1'
-        self.masking_ratio = masking_ratio
-
-        self.encoder = encoder
-        num_patches, encoder_dim = encoder.pos_embedding.shape[-2:]
-        self.to_patch_ground, self.patch_to_emb_ground = encoder.to_patch_embedding[:2]
-        self.to_patch_overhead, self.patch_to_emb_overhead = copy.deepcopy(self.to_patch_ground), copy.deepcopy(self.patch_to_emb_ground)
-        pixel_values_per_patch = self.patch_to_emb_ground.weight.shape[-1]
-        self.iim_token = nn.Parameter(torch.randn(1, encoder_dim))
-        self.enc_embed = nn.Parameter(torch.randn(2*num_patches-1, encoder_dim))
-        self.match = nn.Linear(encoder_dim, 1)
-
-
-        # decoder parameters
-        self.decoder_dim = decoder_dim
-        self.enc_to_dec = nn.Linear(encoder_dim, decoder_dim) if encoder_dim != decoder_dim else nn.Identity()
-        self.mask_token = nn.Parameter(torch.randn(decoder_dim))
-        self.decoder_ground = Transformer(dim = decoder_dim, depth = decoder_depth, heads = decoder_heads, dim_head = decoder_dim_head, mlp_dim = decoder_dim * 4)
-        self.decoder_overhead = copy.deepcopy(self.decoder_ground)
-        #self.decoder_pos_emb = nn.Embedding(num_patches, decoder_dim)
-        self.decoder_pos_emb_ground = nn.Parameter(torch.randn(num_patches-1, decoder_dim))
-        self.decoder_pos_emb_overhead = copy.deepcopy(self.decoder_pos_emb_ground)
-        self.to_pixels_ground = nn.Linear(decoder_dim, pixel_values_per_patch)
-        self.to_pixels_overhead = copy.deepcopy(self.to_pixels_ground)
-        self.out = nn.Sigmoid()
-
-    def forward(self, img_ground, img_overhead):
-        device = img_ground.device
-
-        patches_ground = self.to_patch_ground(img_ground)
-        patches_overhead = self.to_patch_overhead(img_overhead)
-        patches_all = torch.cat((patches_ground, patches_overhead), dim=1)
-        batch, num_patches, *_ = patches_all.shape
-
-        # patch to encoder tokens and add positions
-        tokens_ground = self.patch_to_emb_ground(patches_ground)
-        tokens_overhead = self.patch_to_emb_overhead(patches_overhead)
-
-        tokens_reconstruct = torch.cat((tokens_ground, tokens_overhead), dim=1)
-        tokens_reconstruct = tokens_reconstruct + self.enc_embed[1:]
-
-        tokens_ground_ex = torch.cat((tokens_ground, tokens_ground), dim=0)
-        tokens_overhead_ex = torch.cat((tokens_overhead, torch.roll(tokens_overhead, 1, 0)), dim=0)
-        iim_token = repeat(self.iim_token, 'n d -> b n d', b = 2*batch)
-        tokens_matching = torch.cat((iim_token, tokens_ground_ex, tokens_overhead_ex), dim=1)
-        tokens_matching = tokens_matching + self.enc_embed
-
-        enc_tokens_matching = self.encoder.transformer(tokens_matching)
-        matching_probs = self.match(enc_tokens_matching[:, 0, :])
-        
-        # calculate of patches needed to be masked, and get random indices, dividing it up for mask vs unmasked
-        num_masked = int(self.masking_ratio * num_patches)
-        rand_indices = torch.rand(batch, num_patches, device = device).argsort(dim = -1)
-        masked_indices, unmasked_indices = rand_indices[:, :num_masked], rand_indices[:, num_masked:]
-
-        # get the unmasked tokens to be encoded
-
-        batch_range = torch.arange(batch, device = device)[:, None]
-        tokens_unmasked = tokens_reconstruct[batch_range, unmasked_indices]
-
-        # get the patches to be masked for the final reconstruction loss
-
-        masked_patches = patches_all[batch_range, masked_indices]
-
-        # attend with vision transformer
-
-        encoded_tokens = self.encoder.transformer(tokens_unmasked)
-
-        # project encoder to decoder dimensions, if they are not equal - the paper says you can get away with a smaller dimension for decoder
-
-        decoder_tokens = self.enc_to_dec(encoded_tokens)
-        mask_tokens = repeat(self.mask_token, 'd -> b n d', b = batch, n = num_masked)
-
-        decoder_tokens_all = torch.zeros(batch, num_patches, self.decoder_dim, device = device)
-        decoder_tokens_all[batch_range, unmasked_indices] = decoder_tokens
-        decoder_tokens_all[batch_range, masked_indices] = mask_tokens
-
-        decoder_tokens_ground = decoder_tokens_all[:, :num_patches//2] + self.decoder_pos_emb_ground
-        decoder_tokens_overhead = decoder_tokens_all[:, num_patches//2:] + self.decoder_pos_emb_overhead
-
-        decoded_tokens_ground = self.decoder_ground(decoder_tokens_ground)
-        decoded_tokens_overhead = self.decoder_overhead(decoder_tokens_overhead)
-
-        # splice out the mask tokens and project to pixel values
-        pred_pixel_values_ground = self.out(self.to_pixels_ground(decoded_tokens_ground))
-
-        pred_pixel_values_overhead = self.out(self.to_pixels_overhead(decoded_tokens_overhead))
-
-        pred_pixel_values_all = torch.cat((pred_pixel_values_ground, pred_pixel_values_overhead), dim=1)
-        masked_pixel_preds = pred_pixel_values_all[batch_range, masked_indices]
-
-        loss_recon = F.mse_loss(masked_pixel_preds, masked_patches)
-        matching_labels = torch.cat((torch.ones(batch, device=device), torch.zeros(batch, device=device)))
-        loss_matching = F.binary_cross_entropy_with_logits(matching_probs.squeeze(-1), matching_labels)
-        return loss_recon, loss_matching
 
 class MaeBirds(LightningModule):
     def __init__(self, train_dataset, val_dataset, **kwargs):
         super().__init__()
-        self.vit = ViT(
-            image_size = 224,
-            patch_size = 16,
-            num_classes = 10,
-            dim = 1024,
-            depth = 12,
-            heads = 12,
-            mlp_dim = 2048,
-            dropout=0.2
-        )
-        self.model = MAE(
-            encoder=self.vit,
-            masking_ratio=0.75,
-            decoder_dim=256,
-            decoder_depth=8,
-        )
+        self.model = CrossViewMaskedAutoencoder(img_size1=384, img_size2=224, patch_size1=32, patch_size2=16,
+                 in_chans=3, embed_dim=768, depth=12, num_heads=12,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=partial(nn.LayerNorm, eps=1e-6), norm_pix_loss=False, kernel=3, mlp_hidden_dim=None)
         self.train_dataset = train_dataset
         self.val_dataset = val_dataset
-        self.batch_size = kwargs.get('batch_size', 16)
-        self.num_workers = kwargs.get('num_workers', 16)
+        self.batch_size = kwargs.get('batch_size', 32)
+        self.num_workers = kwargs.get('num_workers', 8)
         self.lr = kwargs.get('lr', 0.02)
+        self.match = nn.Linear(768, 1)
 
     def forward(self, img_ground, img_overhead):
-        return self.model(img_ground, img_overhead)
+        img_ground_ex = torch.cat((img_ground, img_ground), dim=0)
+        img_overhead_ex = torch.cat((img_overhead, torch.roll(img_overhead, 1, 0)), dim=0)
+        embeddings, *_ = self.model.forward_encoder(img_ground_ex, img_overhead_ex, 0)
+        matching_probs = self.match(embeddings[:, 0])
+        matching_labels = torch.cat((torch.ones(img_ground.shape[0], device=self.device), torch.zeros(img_ground.shape[0], device=self.device)))
+        loss_matching = F.binary_cross_entropy_with_logits(matching_probs.squeeze(-1), matching_labels)
+        loss_recon, *_ = self.model(img_ground, img_overhead)
+        return loss_matching, loss_recon
 
     def shared_step(self, batch, batch_idx):
         img_ground, img_overhead = batch[0], batch[1]
         #import code; code.interact(local=locals());
         loss_matching, loss_recon  = self(img_ground, img_overhead)
-        loss = loss_matching + 0.3*loss_recon
+        loss = loss_matching + loss_recon
         return loss, loss_matching, loss_recon
 
     def training_step(self, batch, batch_idx):
-        loss, loss_matching, loss_recon = self.shared_step(batch, batch_idx)
-        self.log('train_loss_recon', loss_recon, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-        self.log('train_loss', loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        self.log('train_loss_matching', loss_matching, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        return {"loss": loss, "loss_matching": loss_matching, "loss_recon":loss_recon}
+        loss, loss_clip, loss_recon = self.shared_step(batch, batch_idx)
+        self.log('train_loss_recon', loss_recon, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log('train_loss', loss, prog_bar=True, on_epoch=True, sync_dist=True)
+        self.log('train_loss_clip', loss_clip, prog_bar=True, on_epoch=True, sync_dist=True)
+        return {"loss": loss, "loss_clip": loss_clip, "loss_recon":loss_recon}
 
     def validation_step(self, batch, batch_idx):
-        loss, loss_matching, loss_recon = self.shared_step(batch, batch_idx)
-        self.log('val_loss_recon', loss_recon, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        self.log('val_loss', loss, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        self.log('val_loss_matching', loss_matching, prog_bar=True, on_step=False, on_epoch=True, sync_dist=True)
-        return {"loss": loss, "loss_matching": loss_matching, "loss_recon":loss_recon}
+        loss, loss_clip, loss_recon = self.shared_step(batch, batch_idx)
+        self.log('val_loss_recon', loss_recon, prog_bar=True, on_epoch=True, sync_dist=True)
+        self.log('val_loss', loss, prog_bar=True, on_epoch=True, sync_dist=True)
+        self.log('val_loss_clip', loss_clip, prog_bar=True, on_epoch=True, sync_dist=True)
+        return {"loss": loss, "loss_clip": loss_clip, "loss_recon":loss_recon}
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
                         shuffle=True,
                         batch_size=self.batch_size,
                         num_workers=self.num_workers,
-                        persistent_workers=True,
+                        persistent_workers=False,
                         pin_memory=True)
 
     def val_dataloader(self):
@@ -197,7 +88,7 @@ class MaeBirds(LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4, weight_decay=0.01)
         scheduler = CosineAnnealingWarmRestarts(optimizer, 40)
-        return [optimizer], [scheduler]
+        return optimizer
 
 class Birds(Dataset):
     def __init__(self, dataset, label, val=False):
@@ -208,23 +99,31 @@ class Birds(Dataset):
         self.val = val
         if not val:
             self.transform_ground = transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.CenterCrop((224, 224)),
-                transforms.ToTensor()
+                transforms.Resize((384, 384)),
+                transforms.RandAugment(7),
+                transforms.AugMix(5, 7),
+                transforms.RandomHorizontalFlip(0.5),
+                transforms.RandomVerticalFlip(0.5),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
             self.transform_overhead = transforms.Compose([
                 transforms.RandomCrop(224),
-                transforms.ToTensor()
+                transforms.ColorJitter(),
+                transforms.RandomHorizontalFlip(0.5),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
         else:
             self.transform_ground = transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.CenterCrop((224, 224)),
-                transforms.ToTensor()
+                transforms.Resize((384, 384)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
             self.transform_overhead = transforms.Compose([
-                transforms.RandomCrop(224),
-                transforms.ToTensor()
+                transforms.Resize(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
         
 
@@ -233,38 +132,39 @@ class Birds(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.images[idx]['file_name']
-        img_ground = Image.open(img_path)
+        img_ground = Image.open(os.path.join('/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/', img_path))
         img_ground = self.transform_ground(img_ground)
         if not self.val:
-            img_overhead = Image.open(f"./train_overhead/images_sentinel/{idx}.jpeg")
+            img_overhead = Image.open(f"/scratch1/fs1/jacobsn/s.sastry/metaformer/train_overhead/images_sentinel/{idx}.jpeg")
         else:
-            img_overhead = Image.open(f"./val_overhead/images_sentinel/{idx}.jpeg")
+            img_overhead = Image.open(f"/scratch1/fs1/jacobsn/s.sastry/metaformer/val_overhead/images_sentinel/{idx}.jpeg")
         img_overhead = self.transform_overhead(img_overhead)
         return img_ground, img_overhead
 
 if __name__=='__main__':
     torch.cuda.empty_cache()
-    logger = WandbLogger(project="Meta-MAE", name="Cross View")
-    train_dataset = json.load(open("train_birds.json"))
-    train_labels = pd.read_csv('train_birds_labels.csv')
+    logger = WandbLogger(project="Cross-View-MAE", name="Pretrained Cross MAE")
+    train_dataset = json.load(open("/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/train_birds.json"))
+    train_labels = pd.read_csv('/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/train_birds_labels.csv')
     train_dataset = Birds(train_dataset, train_labels)
-    val_dataset = json.load(open("val_birds.json"))
-    val_labels = pd.read_csv('val_birds_labels.csv')
+    val_dataset = json.load(open("/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/val_birds.json"))
+    val_labels = pd.read_csv('/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/val_birds_labels.csv')
     val_dataset = Birds(val_dataset, val_labels, val=True)
 
     checkpoint = ModelCheckpoint(
         monitor='val_loss',
         dirpath='checkpoints',
-        filename='CrossMAE-{epoch:02d}-{val_loss:.2f}',
+        filename='CrossMAEv5-{epoch:02d}-{val_loss:.2f}',
         mode='min'
     )
 
     model = MaeBirds(train_dataset, val_dataset)
+    #model = model.load_from_checkpoint('/storage1/fs1/jacobsn/Active/user_s.sastry/metaformer/checkpoints/ContrastiveMAE-epoch=39-val_loss=1.22.ckpt', train_dataset=train_dataset, val_dataset=val_dataset)
     
     trainer = pl.Trainer(
         accelerator='gpu',
         devices=4,
-        strategy='ddp_find_unused_parameters_true',
+        strategy='ddp_find_unused_parameters_false',
         max_epochs=1500,
         num_nodes=1,
         callbacks=[checkpoint],
